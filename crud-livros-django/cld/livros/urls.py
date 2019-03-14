6aebe8f47bfe6b9f0005480b83672e2fc90695a5
rafaelasfernandes <rafaela.sjfernandes@gmail.com>
2019-03-14 15:28:30 -0300

    crud de livros finalizado

--- crud-livros-django/cld/livros/urls.py
+++ crud-livros-django/cld/livros/urls.py
@@ -5,4 +5,8 @@ from .import views
 urlpatterns = [
     path('index', views.index, name='index'),
     path('', views.livro_list, name='livro_list'),
+    path('livro/<int:pk>/', views.livro_detail, name='livro_detail'),
+    path('livro/new', views.livro_new, name='livro_new'),
+    path('livro/<int:pk>/edit/', views.livro_edit, name='livro_edit'),
+    path('livro/<int:pk>/delete/', views.livro_delete, name='livro_delete'),
 ]
--- crud-livros-django/cld/livros/views.py
+++ crud-livros-django/cld/livros/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Livro
+from django.shortcuts import redirect
+from .forms import LivroForm
 
 from . import urls
 # Create your views here.
@@ -10,4 +12,39 @@ def index(request):
 
 def livro_list(request):
     livros = Livro.objects.order_by('titulo')
-    return render(request, 'livros/livro_list.html', {'livros' : livros}, {})
+    return render(request, 'livros/livro_list.html', {'livros':livros})
+
+def livro_detail(request, pk):
+    livro = get_object_or_404(Livro, pk=pk)
+    return render(request, 'livros/livro_detail.html', {'livro':livro})
+
+def livro_new(request):
+    if request.method == "POST":
+        form = LivroForm(request.POST)
+        if form.is_valid():
+            livro = form.save(commit=False)
+            livro.save()
+            return redirect('livro_detail', pk=livro.pk)
+    else:
+        form = LivroForm()
+    return render(request, 'livros/livro_edit.html', {'form':form})
+
+def livro_edit(request, pk):
+    livro = get_object_or_404(Livro, pk=pk)
+    if request.method == "POST":
+        form = LivroForm(request.POST, instance=livro)
+        if form.is_valid():
+            livro = form.save()
+            return redirect('livro_detail', pk=livro.pk)
+    else:
+        form = LivroForm(instance=livro)
+    return render(request, 'livros/livro_edit.html', {'form':form})
+
+def livro_delete(request, pk):
+    livro = get_object_or_404
+    if request.method == "POST":
+        livro.delete()
+        return redirect('livro_list')
+    else:
+        form = LivroForm(instance=livro)
+    return render(request, 'livros/livro_delete.html', {'livro':livro})
