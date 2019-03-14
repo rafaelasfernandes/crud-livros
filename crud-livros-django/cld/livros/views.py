cacf88b4478377bc0493fd2494bfb5ae58c105ce
rafaelasfernandes <rafaela.sjfernandes@gmail.com>
2019-03-14 11:21:34 -0300

    segunda modificacao

--- crud-livros-django/cld/livros/views.py
+++ crud-livros-django/cld/livros/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Livro
 
+from . import urls
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Olá, Mundo. Você está na página index de livros")
+
+def livro_list(request):
+    livros = Livro.objects.order_by('titulo')
+    return render(request, 'livros/livro_list.html', {'livros' : livros}, {})
